2d5f0549b591cc18cc80d5e1dfc979c5b72b47e0
Sujung Choi <schoi0210@gmail.com>
2021-03-19 12:11:22 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [,] multiTable = new int[10,10];
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("\n");
+                for(int j = 1; j <= 10; j++)
+                {
+                    multiTable[i-1, j-1] = i * j;
+                    // Console.WriteLine(multiTable[i-1, j-1]); 
+                        // writeline prints the values in separate line
+
+                    Console.Write((i * j).ToString() + "\t");
+                }
+            }
+        }
+    }
+}
